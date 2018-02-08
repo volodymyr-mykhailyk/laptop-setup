bffb8e2d88934c095028245f1ceb2fd32b7365cd
Volodymyr Mykhailyk <volodymyr.mykhailyk@gmail.com>
2018-02-08 09:53:56 +0200

    Add flycut app

--- sections/install-apps.sh
+++ sections/install-apps.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
+#Applications
 brew cask install google-chrome
 brew cask install firefox
 brew cask install slack
 
+#Development
 brew cask install rubymine
 brew cask install docker
 
 brew cask install virtualbox
+
+#Other tools
+brew cask install flycut
