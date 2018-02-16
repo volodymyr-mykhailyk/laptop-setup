786ed989fc90b81582710129c16068b728b2c39a
Volodymyr Mykhailyk <volodymyr.mykhailyk@gmail.com>
2018-02-16 13:10:08 +0200

    Add telnet for devops tools

--- sections/install-tools.sh
+++ sections/install-tools.sh
@@ -8,6 +8,7 @@ brew install git-crypt
 brew install terraform
 terraform -install-autocomplete
 brew install ansible
+brew install telnet
 
 #Ruby
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
